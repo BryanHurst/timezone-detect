9c6fa493bfbe988e9bc42d9cefa33fb076d6670c
Bryan <bryan@newlinetechnicalinnovations.com>
2014-02-10 17:45:10 -0700

    Look for timezone

--- timezone-detect/middleware.py
+++ timezone-detect/middleware.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 class TimezoneMiddleware(object):
     def process_request(self, request):
-        tz = request.session.get('detected_tz')
+        tz = request.session.get('detected_timezone')
         if tz:
             # `request.timezone_active` is used in the template
             # tag to detect if the timezone has been activated
