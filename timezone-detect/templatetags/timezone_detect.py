1dfaaaa999f12ad4423c5721ba94039abec26e39
Bryan <bryan@newlinetechnicalinnovations.com>
2014-02-10 19:37:23 -0700

    Fix javascript path. Fix template path. Add context to requests.

--- timezone-detect/templatetags/timezone_detect.py
+++ timezone-detect/templatetags/timezone_detect.py
@@ -4,7 +4,7 @@ from django.conf import settings
 register = template.Library()
 
 
-@register.inclusion_tag('timezone-detect/detector.html', takes_context=True)
+@register.inclusion_tag('detector.html', takes_context=True)
 def timezone_detect(context):
     return {
         'show': not hasattr(context['request'], 'timezone_active'),
